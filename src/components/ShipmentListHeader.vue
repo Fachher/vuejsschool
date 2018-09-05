4f3720e3bc29c53530fd6719d9dcf404186da1b6
fachher.syed <fachher@googlemail.com>
2018-09-05 22:39:11 +0200

    Debounce rest call to avoid unnecessary rest calls.

--- src/components/ShipmentListHeader.vue
+++ src/components/ShipmentListHeader.vue
@@ -5,7 +5,7 @@
         <button v-on:click="showHelpMessage">Show help message</button>
 
         <p>
-            <input v-model="searchInputString" v-on:keyup="inputChange">
+            <input v-model="searchInputString" v-on:keyup="onInputChange">
         </p>
 
         <p>You are searching for: {{searchInputString}}</p>
@@ -19,35 +19,44 @@
     import Component from 'vue-class-component'
     import ShipmentListModel from '@/model/ShipmentListModel';
     import {Prop} from "vue-property-decorator";
+    import {debounce} from "lodash";
 
     @Component({
     })
     export default class ShipmentListHeader extends Vue {
-        // private
-        private name:string = "shipmentListHeader";
 
-        // data
-        searchInputString:string = "";
+        /* ============== private fields ================ */
+
+        private debounceInfoParent = debounce(function (this:ShipmentListHeader, event:Event){
+            this.$emit('searchStringChanged', (<HTMLInputElement>event.target).value);
+        }, 800);
+
+        private searchInputString:string = ""; // vuejs data
 
         @Prop()
-        shipmentListModel!: ShipmentListModel;
+        private shipmentListModel!: ShipmentListModel; // vuejs property
 
-        /* ========== methods ================== */
-        showHelpMessage(){
+        /* ============== private methods ================ */
+
+        private showHelpMessage(){
             alert('Search by milestone and mode of transport!')
             // TODO: show modal window with help text
         }
 
-        inputChange(event:Event){
-            this.$emit('searchStringChanged', (<HTMLInputElement>event.target).value);
+        /* =============== event handler ================= */
+
+        onInputChange(event:Event){
+            this.debounceInfoParent(event);
         }
 
-        /* ============ computed properties ===============*/
+        /* ============ computed properties ============== */
+
         get searchInputStringBeautified(){
             return `[${this.searchInputString}]`;
         }
 
-        /* ============== Lifecycle hooks ================= */
+        /* ============== Lifecycle hooks ================ */
+
         mounted(){
             console.log(`${name} has been rendered`);
         }
